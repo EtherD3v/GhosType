76f9a3036172ba7c35f2c7286529c624d6781851
EtherD3v <aymericduparc78@gmail.com>
2025-08-23 17:50:37 +0200

    chore: init freestanding rust binary

---
+++ target/debug/deps/pebble-b77a69d6cc1fecce.d
@@ -0,0 +1,5 @@
+/home/etherd3v/Code/Pebble/target/debug/deps/pebble-b77a69d6cc1fecce: src/main.rs
+
+/home/etherd3v/Code/Pebble/target/debug/deps/pebble-b77a69d6cc1fecce.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/pebble.d
@@ -0,0 +1,1 @@
+/home/etherd3v/Code/Pebble/target/debug/pebble: /home/etherd3v/Code/Pebble/src/main.rs
